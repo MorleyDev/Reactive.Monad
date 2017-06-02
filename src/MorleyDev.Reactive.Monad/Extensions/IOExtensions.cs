bf7772078a2df9d49b6cc39ac3c8a1dbcbe54e84
Jason Morley <morleydev.co.uk@gmail.com>
2017-06-02 12:21:27 +0100

    Added merging of IObservable. Added Concat of IObservable and IEnumerable.

--- src/MorleyDev.Reactive.Monad/Extensions/IOExtensions.cs
+++ src/MorleyDev.Reactive.Monad/Extensions/IOExtensions.cs
@@ -4,23 +4,46 @@ using MorleyDev.Reactive.Monad.Extensions;
 
 namespace MorleyDev.Reactive.Monad.Extensions
 {
-	public static class IOExtensions
+	public static class IOExtensionsNestedEnumerable
 	{
-
 		/// <summary>
-		/// Merge an IO of an Array into a ManyIO
+		/// Merge an IO of an Enumerable into a ManyIO
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="U"></typeparam>
 		/// <param name="self"></param>
 		/// <returns></returns>
-		public static ManyIO<T> Merge<T>(this IO<T[]> self) => self.Select(s => s.ToObservable()).Merge().ToManyIO();
+		public static ManyIO<T> Merge<T, U>(this IO<U> self) where U : IEnumerable<T> => self.Select(s => s.ToObservable()).Merge().ToManyIO();
 
 		/// <summary>
 		/// Merge an IO of an Enumerable into a ManyIO
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="U"></typeparam>
 		/// <param name="self"></param>
 		/// <returns></returns>
-		public static ManyIO<T> Merge<T>(this IO<IEnumerable<T>> self) => self.Select(s => s.ToObservable()).Merge().ToManyIO();
+		public static ManyIO<T> Concat<T, U>(this IO<U> self) where U : IEnumerable<T> => self.Select(s => s.ToObservable()).Concat().ToManyIO();
+	}
+
+	public static class IOExtensionsNestedObservable
+	{
+
+		/// <summary>
+		/// Merge an IO of an Enumerable into a ManyIO
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="U"></typeparam>
+		/// <param name="self"></param>
+		/// <returns></returns>
+		public static ManyIO<T> Merge<T, U>(this IO<U> self) where U : IObservable<T> => self.SelectMany(s => s).ToManyIO();
+
+		/// <summary>
+		/// Merge an IO of an Enumerable into a ManyIO
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="U"></typeparam>
+		/// <param name="self"></param>
+		/// <returns></returns>
+		public static ManyIO<T> Concat<T, U>(this IO<U> self) where U : IObservable<T> => self.Select(s => (IObservable<T>)s).Concat().ToManyIO();
 	}
 }
