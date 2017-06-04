752214cebe39e13d2b295aaf16f07ffaddcc6ce2
Jason Morley <morleydev.co.uk@gmail.com>
2017-06-04 16:37:45 +0100

    Added Observable -> ToMaybeIO extension

--- src/MorleyDev.Reactive.Monad/Extensions/ObservableExtensions.cs
+++ src/MorleyDev.Reactive.Monad/Extensions/ObservableExtensions.cs
@@ -7,5 +7,7 @@ namespace MorleyDev.Reactive.Monad.Extensions
 		public static IO<T> ToIO<T>(this IObservable<T> self) => IO<T>.From(self);
 
 		public static ManyIO<T> ToManyIO<T>(this IObservable<T> self) => ManyIO<T>.From(self);
+
+		public static MaybeIO<T> ToMaybeIO<T>(this IObservable<T> self) => MaybeIO.ToMaybeIO(self);
 	}
 }
