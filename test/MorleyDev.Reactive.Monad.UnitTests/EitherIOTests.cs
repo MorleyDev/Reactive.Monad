3229062014f6670078124c0e4400a76f0fb68e5c
Jason Morley <morleydev.co.uk@gmail.com>
2017-07-26 20:15:17 +0100

    Fixed tests

--- test/MorleyDev.Reactive.Monad.UnitTests/EitherIOTests.cs
+++ test/MorleyDev.Reactive.Monad.UnitTests/EitherIOTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using MorleyDev.Reactive.Monad.Extra;
 
 namespace MorleyDev.Reactive.Monad.UnitTests
 {
@@ -11,11 +12,11 @@ namespace MorleyDev.Reactive.Monad.UnitTests
 		[Fact]
 		public async Task BasicTest()
 		{
-			IO<Either<int, double>> left = IO.From(() => (Either<int, double>)Either.Left(10));
+			IO<Either<int, double>> left = IO.Run(() => (Either<int, double>)Either.Left(10));
 			(await left.Lhs().ToList()).Should().BeEquivalentTo(new[] { 10 });
 			(await left.Rhs().ToArray()).Should().BeEmpty();
 
-			IO<Either<int, double>> right = IO.From(() => (Either<int, double>)Either.Right(25.0));
+			IO<Either<int, double>> right = IO.Run(() => (Either<int, double>)Either.Right(25.0));
 			(await right.Rhs().ToList()).Should().BeEquivalentTo(new[] { 25.0 });
 			(await right.Lhs().ToList()).Should().BeEmpty();
 
@@ -24,21 +25,21 @@ namespace MorleyDev.Reactive.Monad.UnitTests
 		[Fact]
 		public async Task MatchTest()
 		{
-			IO<Either<int, double>> left = IO.From(() => (Either<int, double>)Either.Left(10));
+			IO<Either<int, double>> left = IO.Run(() => (Either<int, double>)Either.Left(10));
 			(await left.Match(x => x * 2.5, y => y * 2)).Should().Be(25.0);
 
-			IO<Either<int, double>> right = IO.From(() => (Either<int, double>)Either.Right(25.0));
+			IO<Either<int, double>> right = IO.Run(() => (Either<int, double>)Either.Right(25.0));
 			(await right.Match(x => x * 2.5, y => y * 2)).Should().Be(50.0);
 		}
 
 		[Fact]
 		public async Task MatchMapTest()
 		{
-			IO<Either<int, double>> left = IO.From(() => (Either<int, double>)Either.Left(10));
+			IO<Either<int, double>> left = IO.Run(() => (Either<int, double>)Either.Left(10));
 			(await left.MatchMap(l => l.ToString(), r => (int)(r / 2.5)).Lhs()).Should().Be("10");
 			(await left.MatchMap(l => l.ToString(), r => (int)(r / 2.5)).Rhs().IsEmpty()).Should().Be(true);
 
-			IO<Either<int, double>> right = IO.From(() => (Either<int, double>)Either.Right(25.0));
+			IO<Either<int, double>> right = IO.Run(() => (Either<int, double>)Either.Right(25.0));
 			(await right.MatchMap(l => l.ToString(), r => (int)(r / 2.5)).Rhs()).Should().Be(10);
 			(await right.MatchMap(l => l.ToString(), r => (int)(r / 2.5)).Lhs().IsEmpty()).Should().Be(true);
 
@@ -48,36 +49,36 @@ namespace MorleyDev.Reactive.Monad.UnitTests
 			(await left.MatchMap(e => Either.Right(e.ToString()), e => Either.Left(e * 0.5)).Rhs()).Should().Be("10");
 			(await left.MatchMap(e => Either.Right(e.ToString()), e => Either.Left(e * 0.5)).Lhs().IsEmpty()).Should().Be(true);
 
-			(await right.MatchMap(e => Either.Left(e.ToString()), e => Either.Right((int)(e * 0.5))).Rhs()).Should().Be(5);
+			(await right.MatchMap(e => Either.Left(e.ToString()), e => Either.Right((int)(e * 0.5))).Rhs()).Should().Be(12);
 			(await right.MatchMap(e => Either.Left(e.ToString()), e => Either.Right((int)(e * 0.5))).Lhs().IsEmpty()).Should().Be(true);
-			(await right.MatchMap(e => Either.Right(e.ToString()), e => Either.Left((int)(e * 0.5))).Lhs()).Should().Be(5);
+			(await right.MatchMap(e => Either.Right(e.ToString()), e => Either.Left((int)(e * 0.5))).Lhs()).Should().Be(12);
 			(await right.MatchMap(e => Either.Right(e.ToString()), e => Either.Left((int)(e * 0.5))).Rhs().IsEmpty()).Should().Be(true);
 		}
 
 		[Fact]
 		public async Task MatchManyTest()
 		{
-			IO<Either<int, double>> left = IO.From(() => (Either<int, double>)Either.Left(10));
+			IO<Either<int, double>> left = IO.Run(() => (Either<int, double>)Either.Left(10));
 			(await left.MatchMany(x => Observable.Return(x * 2.5), y => Observable.Return(y * 2))).Should().Be(25.0);
-			(await left.MatchMany(x => IO.From(() => x * 2.5), y => IO.From(() => y * 2))).Should().Be(25.0);
-			(await left.MatchMany(x => MaybeIO.From(() => Maybe.Just(x * 2.5)), y => MaybeIO.From(() => Maybe.Just(y * 2)))).Should().Be(25.0);
+			(await left.MatchMany(x => IO.Run(() => x * 2.5), y => IO.Run(() => y * 2))).Should().Be(25.0);
+			(await left.MatchMany(x => MaybeIO.Run(() => Maybe.Just(x * 2.5)), y => MaybeIO.Run(() => Maybe.Just(y * 2)))).Should().Be(25.0);
 			(await left.MatchMany(x => Observable.Return(x * 2.5), y => Observable.Return(y * 2))).Should().Be(25.0);
 
-			IO<Either<int, double>> right = IO.From(() => (Either<int, double>)Either.Right(25.0));
+			IO<Either<int, double>> right = IO.Run(() => (Either<int, double>)Either.Right(25.0));
 			(await right.MatchMany(x => Observable.Return(x * 2.5), y => Observable.Return(y * 2))).Should().Be(50.0);
-			(await right.MatchMany(x => IO.From(() => x * 2.5), y => IO.From(() => y * 2))).Should().Be(50.0);
-			(await right.MatchMany(x => MaybeIO.From(() => Maybe.Just(x * 2.5)), y => MaybeIO.From(() => Maybe.Just(y * 2)))).Should().Be(50.0);
+			(await right.MatchMany(x => IO.Run(() => x * 2.5), y => IO.Run(() => y * 2))).Should().Be(50.0);
+			(await right.MatchMany(x => MaybeIO.Run(() => Maybe.Just(x * 2.5)), y => MaybeIO.Run(() => Maybe.Just(y * 2)))).Should().Be(50.0);
 			(await right.MatchMany(x => Observable.Return(x * 2.5), y => Observable.Return(y * 2))).Should().Be(50.0);
 		}
 
 		[Fact]
 		public async Task MatchManyMapTest()
 		{
-			IO<Either<int, double>> left = IO.From(() => (Either<int, double>)Either.Left(10));
+			IO<Either<int, double>> left = IO.Run(() => (Either<int, double>)Either.Left(10));
 			(await left.MatchManyMap(l => IO.Run(() => l.ToString()), r => IO.Run(() => (int)(r / 2.5))).Lhs()).Should().Be("10");
 			(await left.MatchManyMap(l => IO.Run(() => l.ToString()), r => IO.Run(() => (int)(r / 2.5))).Rhs().IsEmpty()).Should().Be(true);
 
-			IO<Either<int, double>> right = IO.From(() => (Either<int, double>)Either.Right(25.0));
+			IO<Either<int, double>> right = IO.Run(() => (Either<int, double>)Either.Right(25.0));
 			(await right.MatchManyMap(l => IO.Run(() => l.ToString()), r => IO.Run(() => (int)(r / 2.5))).Rhs()).Should().Be(10);
 			(await right.MatchManyMap(l => IO.Run(() => l.ToString()), r => IO.Run(() => (int)(r / 2.5))).Lhs().IsEmpty()).Should().Be(true);
 		}
